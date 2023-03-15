25b7d6dc8a94935c26fdd788d6b69d3792c0e1e9
NNOROMIV <nnorom.prince44@gmail.com>
2023-03-15 09:08:09 +0100

    Delete DataFrames.R
    Already a section of a file

--- DataFrames.R
+++
@@ -1,8 +0,0 @@
-# Create the data frame.
-BMI <- 	data.frame(
-   gender = c("Male", "Male","Female"), 
-   height = c(152, 171.5, 165), 
-   weight = c(81,93, 78),
-   Age = c(42,38,26)
-)
-print(BMI)
